77508d6bfdb1f820c635285ced7b2be78363b5e0
Dan Tsiopych <dania2008dd@icloud.com>
2024-07-27 13:57:02 +0300

    version 2.6

--- src/main.jsx
+++ src/main.jsx
@@ -4,22 +4,22 @@ import App from './App.jsx'
 import { QueryClient, QueryClientProvider } from 'react-query'
 
 const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        slateTime: 1000 * 60 * 5,
-        gcTime: 1000 * 60 * 10,
-        refetchOnWindowFocus: false,
-      }
+  defaultOptions: {
+    queries: {
+      slateTime: 1000 * 60 * 5,
+      gcTime: 1000 * 60 * 10,
+      refetchOnWindowFocus: false,
     }
+  }
 })
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  
+
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-    <App />
-  </QueryClientProvider>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
-  
+
 )
